7b47f6d4e9fb119e0fc95a591c273dc13fe5eeed
Jennifer Fajardo <jafajardo83@misena.edu.co>
2021-05-31 16:54:39 -0500

    Carga del proyecto

---
+++ src/main/java/com/proyecto2242758/proyectospring/ProyectospringApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto2242758.proyectospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto2242758/proyectospring/ProyectospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto2242758.proyectospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
